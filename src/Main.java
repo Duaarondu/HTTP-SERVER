da49799576d86b65e2e4e46d3db5bc4464bfee9c
Aaron Du <duaarondu@gmail.com>
2025-12-29 18:19:42 -0500

    Connection to HTTP server

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Establishing for connection on port 8080");
+        while (true){
+            final Socket client = server.accept();
+            InputStreamReader isr
+                    = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while (!line.isEmpty()){
+                System.out.println(line);
+                line = reader.readLine();
+
+            }
+
+        }
+        }
+    }
